27d4fd62b62acf1929996fab78d4373bacef9b7a
Nelson Lin <nelson.lin.0416@gmail.com>
2024-04-28 15:16:50 -0700

    Project 2

---
+++ Crowdfunding DB.sql
@@ -0,0 +1,48 @@
+-- Campaign Table
+Create Table campaign (
+	cd_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR(100) NOT NULL,
+	description VARCHAR(100) NOT NULL,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
+	outcome VARCHAR(20) NOT NULL,
+	backers_count INT NOT NULL,
+	country VARCHAR(2) NOT NULL,
+	currency VARCHAR(3) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR(4) NOT NULL,
+	subcategory_id VARCHAR(10) NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+-- Category Table
+Create Table category (
+	category_id VARCHAR(4) PRIMARY KEY,
+	category VARCHAR(20) NOT NULL
+);
+
+-- Subcategory Table
+Create Table subcategory (
+	subcategory_id VARCHAR(10) PRIMARY KEY,
+	subcategory VARCHAR(50) NOT NULL
+);
+
+-- Contacts Table
+Create Table contacts (
+	contact_id INT PRIMARY KEY,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	email VARCHAR(100) NOT NULL
+);
+
+Select * From campaign
+
+Select * From category
+
+Select * From subcategory
+
+Select * From contacts
